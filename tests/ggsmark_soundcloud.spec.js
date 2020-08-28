46acc27e9b65cd70cc188163efcb566cf33678f6
Kaptian Core <kaptiancore@gmail.com>
2020-08-28 16:40:39 +1000

    Update ggsmark_soundcloud.spec.js

--- tests/ggsmark_soundcloud.spec.js
+++ tests/ggsmark_soundcloud.spec.js
@@ -17,18 +17,17 @@ import ggsmark from '../src/ggsmark_soundcloud'
 describe('render soundcloud blocks', () => {
   test('soundcloud iframe test', () => {
     // Arrange
-    let string = `:soundloud https://soundcloud.com/iamcardib/wap-feat-megan-thee-stallion`
+    let string = `:soundcloud https://soundcloud.com/iamcardib/wap-feat-megan-thee-stallion`
 
     // Act
     let result = ggsmark(string)
 
     // Assert
     expect(result).toBe(`
-    <div class=\\"soundcloud_song\\">
-    <iframe width=\\"100%" height=\\"166" scrolling=\\"no" frameborder=\\"no" src=\\"https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F871426135&show_artwork=true&maxheight=166"></iframe>
+    <div class=\"soundcloud_song\">
+    <iframe width=\"100%" height=\"166" scrolling=\"no" frameborder=\"no" src=\"https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F871426135&show_artwork=true&maxheight=166"></iframe>
     <p>:soundloud https://soundcloud.com/iamcardib/wap-feat-megan-thee-stallion</p>
     </div>
-    "
     `)
   })
 })
