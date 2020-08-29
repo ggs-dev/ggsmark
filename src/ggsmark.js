de2e64e3dcae283df382d180f40f507af4f9bede
Johnny Huynh <info@johnnyhuy.com>
2020-08-29 12:47:17 +1000

    Get Axios and async working

--- src/ggsmark.js
+++ src/ggsmark.js
@@ -1,7 +1,8 @@
 import { HtmlRenderer, Parser, Node } from 'commonmark'
 import DOMPurify from 'dompurify'
+import axios from 'axios'
 
-export default function (text) {
+export default async function (text) {
   let reader = new Parser()
   let writer = new HtmlRenderer()
   let parsed = reader.parse(text)
@@ -50,7 +51,46 @@ export default function (text) {
 
       node.unlink()
     }
-    // if (node.type === 'text' && !!node.parent && node.parent.type === 'paragraph')
+
+    if (
+      node.type === 'text' &&
+      !!node.parent &&
+      node.parent.type === 'paragraph' &&
+      node.literal.match(/\:soundcloud/)
+    ) {
+      let nestedEvent = walker.next()
+      let nestedNode = nestedEvent.node
+
+      // Make sure text nodes are not fragmented
+      while (!!nestedNode.prev && nestedNode.type === 'text') {
+        nestedNode.prev.literal = nestedNode.prev.literal + nestedNode.literal
+        nestedNode.unlink()
+        nestedEvent = walker.next()
+        nestedNode = nestedEvent.node
+      }
+
+      let matchSoundCloudExp = /^(?:soundcloud|sc)\s((?:https?\:\/\/)?(?:www\.)?(?:soundcloud\.com\/)[^&#\s\?]+\/[^&#\s\?]+)/
+      let splitText = node.literal.split(matchSoundCloudExp)
+
+      for (let index in splitText) {
+        if (index % 2 == 0) {
+          let text = new Node('text')
+          text.literal = splitText[index]
+          node.insertBefore(text)
+        } else {
+          let div = new Node('custom_block')
+          div.onEnter = '<div class="soundcloud">'
+          div.onExit = '</div>'
+          let iframe = new Node('custom_inline')
+          iframe.onEnter = `<iframe src="https://www.youtube.com/embed/${splitText[index]}" type="text/html" frameborder="0">`
+          iframe.onExit = '</iframe>'
+          div.appendChild(iframe)
+          node.insertBefore(div)
+        }
+      }
+
+      node.unlink()
+    }
   }
 
   return DOMPurify.sanitize(writer.render(parsed), { ADD_TAGS: ['iframe'] })
