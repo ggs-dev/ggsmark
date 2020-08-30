ea1fd244915ef2feee7a9e5e3f121ee41a64a4bb
Johnny Huynh <info@johnnyhuy.com>
2020-08-30 19:15:58 +1000

    Make tests fail because it was invalid

--- tests/ggsmark.spec.js
+++ tests/ggsmark.spec.js
@@ -55,21 +55,21 @@ describe('render centered text', () => {
     let string = `:text-center yeet :text-center`
     let result = ggsmark(string)
 
-    expect(result).toContain(``) // TODO
+    expect(result).toBe(``) // TODO
   })
 
   test('text center with some text before and after', () => {
     let string = `this should not be in text center :text-center yeet :text-center after text`
     let result = ggsmark(string)
 
-    expect(result).toContain(``) // TODO
+    expect(result).toBe(``) // TODO
   })
 
   test('text center in text center', () => {
     let string = ':text-center :text-center wtf :text-center :text-center'
     let result = ggsmark(string)
 
-    expect(result).toContain(``) // TODO
+    expect(result).toBe(``) // TODO
   })
 })
 
