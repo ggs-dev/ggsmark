b13f119303dc132241967f0b1caf27cba7d57d18
Kaptian Core <kaptiancore@gmail.com>
2020-08-30 17:44:41 +1000

    Update ggsmark.js

--- src/ggsmark.js
+++ src/ggsmark.js
@@ -50,7 +50,6 @@ export default function (text) {
 
       node.unlink()
     }
-    // if (node.type === 'text' && !!node.parent && node.parent.type === 'paragraph')
     else if(
     node.type === 'text' &&
     !!node.parent &&
@@ -79,6 +78,41 @@ export default function (text) {
       } else {
         let div = new Node('custom_block')
         div.onEnter = `<div style="text-align: center"> ${splitText[index]}`
+        div.onExit = '</div>'
+        node.insertBefore(div)
+      }
+    }
+
+    node.unlink()
+  }
+  else if(
+    node.type === 'text' &&
+    !!node.parent &&
+    node.parent.type === 'paragraph' &&
+    node.literal.match(/\:text-right)
+  ) {
+    let nestedEvent = walker.next()
+    let nestedNode = nestedEvent.node
+
+    // Make sure text nodes are not fragmented
+    while (!!nestedNode.prev && nestedNode.type === 'text') {
+      nestedNode.prev.literal = nestedNode.prev.literal + nestedNode.literal
+      nestedNode.unlink()
+      nestedEvent = walker.next()
+      nestedNode = nestedEvent.node
+    }
+
+    let matchTextCntrExp = (/\:text-right/)
+    let splitText = node.literal.split(matchTextCntrExp)
+
+    for (let index in splitText) {
+      if (index % 2 == 0) {
+        let text = new Node('text')
+        text.literal = splitText[index]
+        node.insertBefore(text)
+      } else {
+        let div = new Node('custom_block')
+        div.onEnter = `<div style="text-align: right"> ${splitText[index]}`
         div.onExit = '</div>'
         node.insertBefore(div)
       }
