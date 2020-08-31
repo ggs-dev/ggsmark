4b2dbbbc669e4140bf6a8663e45ad005b5fe451e
Kaptian Core <kaptiancore@gmail.com>
2020-08-31 10:00:49 +1000

    Update ggsmark.spec.js

--- tests/ggsmark.spec.js
+++ tests/ggsmark.spec.js
@@ -1,5 +1,74 @@
 import ggsmark from '../src/ggsmark'
 
+describe('render centered text', () => {
+  test('single line text center', () => {
+    let string = ':text-center Test123'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('wrap text center', () => {
+    let string = `:text-center yeet :text-center`
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('text center with some text before and after', () => {
+    let string = `this should not be in text center :text-center yeet :text-center after text`
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('text center in text center', () => {
+    let string = ':text-center test :text-center wtf :text-center test :text-center'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('use bold text', () => {
+    let string = '\n\n:text-center\n\n**test** \n:text-center'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('use bold text single line', () => {
+    let string = ':text-center **bold**'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+
+  test('use heading', () => {
+    let string = '\n\n:text-center\n# test\n:text-center'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+})
+
+describe('render right allignment text', () => {
+  test('text-right', () => {
+    let string = ':text-right Test123'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+})
+
+describe('render left allignment text', () => {
+  test('text-left', () => {
+    let string = ':text-left Test123'
+    let result = ggsmark(string)
+
+    expect(result).toMatchSnapshot()
+  })
+})
+
 describe('render soundcloud blocks', () => {
   test('single line', () => {
     // Arrange
@@ -75,75 +144,6 @@ new line
     let result = ggsmark(string)
 
     // Assert
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('render centered text', () => {
-  test('single line text center', () => {
-    let string = ':text-center Test123'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('wrap text center', () => {
-    let string = `:text-center yeet :text-center`
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('text center with some text before and after', () => {
-    let string = `this should not be in text center :text-center yeet :text-center after text`
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('text center in text center', () => {
-    let string = ':text-center test :text-center wtf :text-center test :text-center'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('use bold text', () => {
-    let string = '\n\n:text-center\n\n**test** \n:text-center'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('use bold text single line', () => {
-    let string = ':text-center **bold**'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  test('use heading', () => {
-    let string = '\n\n:text-center\n# test\n:text-center'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('render right allignment text', () => {
-  test('text-right', () => {
-    let string = ':text-right Test123'
-    let result = ggsmark(string)
-
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('render left allignment text', () => {
-  test('text-left', () => {
-    let string = ':text-left Test123'
-    let result = ggsmark(string)
-
     expect(result).toMatchSnapshot()
   })
 })
