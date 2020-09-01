754b725884dfe3c17b85f9d1efe1a3825ac6b2e1
Johnny Huynh <info@johnnyhuy.com>
2020-09-02 09:06:45 +1000

    Get SoundCloud and YouTube working

--- src/ggsmark.js
+++ src/ggsmark.js
@@ -1,14 +1,16 @@
 import unified from 'unified'
-import parse from 'remark-parse'
+import markdown from 'remark-parse'
 import stringify from 'rehype-stringify'
 import rehype from 'remark-rehype'
 import iframe from 'remark-iframes'
 
+// Import this since remark-iframe needs it
+import 'regenerator-runtime/runtime'
+
 export default (text) => {
   return unified()
-    .use(parse)
+    .use(markdown)
     .use(iframe, {
-      // Youtube RegEx example
       'www.youtube.com': {
         tag: 'iframe',
         width: 560,
@@ -24,14 +26,37 @@ export default (text) => {
         },
         removeAfter: '&'
       },
-      // Youtube oEmbed example
       'youtu.be': {
+        tag: 'iframe',
         width: 560,
         height: 315,
         disabled: false,
-        oembed: 'https://www.youtube.com/oembed'
+        replace: [
+          ['watch?v=', 'embed/'],
+          ['http://', 'https://']
+        ],
+        thumbnail: {
+          format: 'http://img.youtube.com/vi/{id}/0.jpg',
+          id: '.+/(.+)$'
+        },
+        removeAfter: '&'
+      },
+      'soundcloud.com': {
+        tag: 'iframe',
+        width: '100%',
+        height: 150,
+        disabled: false,
+        replace: [
+          [
+            'soundcloud.com/',
+            'w.soundcloud.com/player/?visual=true&url=https://soundcloud.com/'
+          ],
+          ['http://', 'https://']
+        ]
       }
     })
     .use(rehype)
     .use(stringify)
+    .processSync(text)
+    .toString()
 }
