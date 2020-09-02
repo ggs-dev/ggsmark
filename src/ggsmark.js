957a1fcb845fca38e8948eba4de926fdc6bc36a0
Kaptian Core <34361379+KaptianCore@users.noreply.github.com>
2020-09-03 07:47:25 +1000

    Update src/ggsmark.js
    Co-authored-by: Johnny Huynh <info@johnnyhuy.com>

--- src/ggsmark.js
+++ src/ggsmark.js
@@ -3,7 +3,7 @@ import markdown from 'remark-parse'
 import stringify from 'rehype-stringify'
 import rehype from 'remark-rehype'
 import iframe from 'remark-iframes'
-const remarkAlign = require('remark-align')
+import remarkAlign from 'remark-align'
 
 // Import this since remark-iframe needs it
 import 'regenerator-runtime/runtime'
