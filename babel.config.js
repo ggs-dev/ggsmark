bf2ecd21c81d11522b13b3631bfd1ef877244271
Johnny Huynh <info@johnnyhuy.com>
2020-09-13 18:16:21 +1000

    fix: don't ignore tests in main Babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   presets: ['@babel/preset-env'],
-  ignore: ['**/*.spec.js', '**/*.test.js'],
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-modules-commonjs'
