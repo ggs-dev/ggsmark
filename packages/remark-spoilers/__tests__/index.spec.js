b678a8efb03975e8aa652600aa504e6c9d90fd1d
Johnny Huynh <info@johnnyhuy.com>
2020-09-13 11:04:08 +1000

    test: updated tests to reflect spoilers

--- packages/remark-spoilers/__tests__/index.spec.js
+++ packages/remark-spoilers/__tests__/index.spec.js
@@ -4,12 +4,69 @@ import color from '..'
 import dedent from 'dedent'
 
 describe('spoilers', () => {
-  test('should not color if token never ends', () => {
+  test('should show spoiler without custom summary', () => {
     // Arrange
     let string = dedent`
-    !# red
+    !spoiler
+    peek-a-boo!
+    !spoiler
+    `
+
+    // Act
+    let result = remark().use(html).use(color).processSync(string).toString()
+
+    // Assert
+    expect(result).toMatchSnapshot()
+  })
+
+  test('should show spoiler with custom summary', () => {
+    // Arrange
+    let string = dedent`
+    !spoiler Click me to see a surprise
+    peek-a-boo!
+    !spoiler
+    `
+
+    // Act
+    let result = remark().use(html).use(color).processSync(string).toString()
+
+    // Assert
+    expect(result).toMatchSnapshot()
+  })
+
+  test('should show multiple spoilers', () => {
+    // Arrange
+    let string = dedent`
+    # Hello world
+
+    This is some text.
+
+    !spoiler Click me to see a surprise
+    peek-a-boo!
+    !spoiler
+
+    between the spoilers
+
+    !spoiler Another click me to see a surprise
+    peek-a-boo!
+    !spoiler
+
+    the end
+    `
+
+    // Act
+    let result = remark().use(html).use(color).processSync(string).toString()
+
+    // Assert
+    expect(result).toMatchSnapshot()
+  })
+
+  test('should not show spoiler if not closed', () => {
+    // Arrange
+    let string = dedent`
+    !spoiler
     
-    this should not be red
+    why isn't this working!?
 
     there's no end!!!
     `
